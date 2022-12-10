dd5a7d23994a25b108d3120d016a2b2f92aeaef9
Filip <filipwedzicha@gmail.com>
2022-12-10 00:09:09 +0000

    Create a main.go file to run the recommenders

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/1fxe/board-game-recommender-system/recommenders"
+	"github.com/1fxe/board-game-recommender-system/shared"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	// TODO Database connection
+	file, err := os.ReadFile("data/test_game_data.json")
+	if err != nil {
+		log.Panicln("Error reading file", err)
+	}
+
+	var boardGames []shared.BoardGame
+	err = json.Unmarshal(file, &boardGames)
+	if err != nil {
+		log.Panicln("Error unmarshalling json", err)
+	}
+
+	// Give each Game a fake ID
+	for i := range boardGames {
+		boardGames[i].ID = i
+	}
+
+	var users []shared.User
+	file, err = os.ReadFile("data/test_user_data.json")
+	if err != nil {
+		log.Panicln("Error reading file", err)
+	}
+	err = json.Unmarshal(file, &users)
+
+	contentBasedRecommender := recommenders.ContentBasedRecommender{}
+	contentBasedRecommender.PopulateBoardGames(boardGames)
+	recommendations := contentBasedRecommender.RecommendBoardGames(boardGames[0], 0.4, shared.Characteristic{
+		Categories: []shared.Data{
+			{
+				Name:        "Wargame",
+				Description: "are games that depict military actions. \u003cem\u003eWargames\u003c",
+			},
+		},
+	})
+
+	// shared.PrettyPrintRecommendations(recommendations)
+
+	collaborativeRecommender := recommenders.CollaborativeRecommender{}
+	collaborativeRecommender.PopulateBoardGames(boardGames)
+	collaborativeRecommender.PopulateUsers(users)
+	theUser := rand.Intn(len(users))
+
+	shared.PrintUserData(users[theUser], boardGames)
+
+	recommendations = collaborativeRecommender.RecommendBoardGames(theUser)
+
+	shared.PrettyPrintRecommendations(recommendations)
+}
